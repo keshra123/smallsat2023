91dcc1ee1008249783c84a4fc33083ac3d87c815
keshra123 <keshra123@gmail.com>
2023-04-09 00:37:50 -0400

    New File

---
+++ password.c
@@ -0,0 +1,30 @@
+//C Program to Validate Password
+
+#include<stdio.h>
+#include <stdbool.h>
+#include<string.h>
+
+char pwd[25] = "ssrluga23";
+bool passwordcorrect = false;
+
+void passwordcheck(){
+char entered[25]; 
+printf("Hello, please enter your password:\n");
+gets(entered);
+//bool local = false;
+if (strcmp(pwd,entered)!=0)
+printf("It doesn't work\n");
+else {
+passwordcorrect = true;
+printf("It worked");
+//local = true;
+} 
+//return passwordcorrect;
+}
+
+void main()
+{
+while(passwordcorrect == false)
+passwordcheck();
+//getch();
+}
